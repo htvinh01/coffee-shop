89309033e5455a514c29ab431f43f96f51c6b326
Cris <Trungtvinhl107@gmail.com>
2022-01-18 01:30:02 +0700

    Done!

---
+++ src/coffee_project/NewClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coffee_project;
+
+import java.awt.Font;
+import java.util.Date;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.border.EmptyBorder;
+
+/**
+ *
+ * @author ASUS
+ */
+public class NewClass {
+    
+    public static void main(String args[]){
+  Date date=(Date) java.util.Calendar.getInstance().getTime();   System.out.println(date); 
+    }
+}
